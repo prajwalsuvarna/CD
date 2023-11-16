598fc2d571a8508b6955c6f8abdeea0ac45a8699
Prajwal H Suvarna <prajwalsuvarna1906@gmail.com>
2023-11-16 16:21:12 +0530

    source code 🌐

---
+++ lexical_analyzer.py
@@ -0,0 +1,114 @@
+import re
+# REGEXs TO MATCH
+regex_digit = '^[0-9]+$'
+regex_float = '[+-]?([0-9]*[.])?[0-9]+[E-]?[E+]?[0-9]'
+regex_names = '[a-zA-Z][a-zA-Z0-9]*'
+regex_string = '["][ ]*[a-zA-Z0-9 ]*[ ]*["]'
+SYMBOLS = ['(',
+           ')',
+           ';',
+            ',',
+            ':',
+            '\'',
+            '[',
+            ']']
+KEYWORDS = {'t': ['INTEGER', 'REAL','STRING','int'],
+            'm': ['main'],
+            'b': ['BEGIN','Begin','begin'],
+            'p': ['PRINT'],
+            'e': ['END','End','end'],
+            's': [' '," "],
+            'o': ['+', '-', ':=','=','>','/'],
+            'n': ['\n'],
+            'f': ['FOR','for'],
+            'u': ['TO','to'],
+            'z': ['do'],
+            'i': ['if'],
+            'y': ['endif','endfor'],
+            'd': ['2','4','6','10','0','-3.56E-8','4.567','"text1"','"hello"','1','5','"HELLO"','"Strings"'],
+            'w': ['while'],
+            'r':['return']
+            }
+def getIndex(word):
+    keys = list(KEYWORDS.keys())
+    values = list(KEYWORDS.values())
+    for value in values:
+        if word in value:
+            i = values.index(value)
+            return keys[i]
+
+# TO ADD : SUPPORT FOR STRINGS WITH MULTIPLE WORDS (>2)
+def generate_tokens2(filename):
+    tokens = []
+    str_occ=''
+    flag =False
+    F = open(filename,"r")
+    for line in F:
+        linelist = line.split()
+        for i in range(len(linelist)):
+            word = linelist[i]
+            j=i
+            if(word[0] == '"'):
+                flag = True
+                # while(j<len(linelist)):'
+                while (flag):
+                    if(linelist[j][-1] == '"'):
+                        # print(linelist[j])
+                        str_occ += linelist[j] + " "
+                        # print(str_occ)
+                        tokens.append ('d')
+                        str_occ=''
+                        flag = False
+                        # break
+                    else:
+                        str_occ += linelist[j] + " "
+                        j +=1
+            # word = str_occ
+                continue
+            i=j+1
+            token = getIndex(word)
+            if(re.search(regex_digit, word)):
+                tokens.append('d')
+                continue
+            if(re.search(regex_float , word)):
+                tokens.append('d')
+                continue
+            if token in KEYWORDS:
+                tokens.append(token)
+            elif word in SYMBOLS:
+                tokens.append(word)
+            elif(re.search(regex_names, word)):
+                tokens.append('v')
+                continue
+
+    token_string = "".join(tokens)
+    print(token_string)
+    return token_string
+
+
+def generate_tokens(filename):
+    tokens = []
+    F = open(filename, "r")
+    for line in F:
+        for word in line.split():
+            token = getIndex(word)
+            if(re.search(regex_digit, word)):
+                tokens.append('d')
+                continue
+            if(re.search(regex_float, word)):
+                tokens.append('d' )
+                continue
+            if token in KEYWORDS:
+                tokens.append(token)
+            elif word in SYMBOLS:
+                tokens.append(word)
+            elif(re.search(regex_names, word)):
+                tokens.append('v')
+                continue
+    token_string = "".join(tokens)
+    print("Token are\n")
+    print(token_string)
+    return token_string
+
+
+
